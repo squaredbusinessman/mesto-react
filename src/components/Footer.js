3efc3780109bf9fce9aa95376e2c8b3464d510f6
squaredbusinessman <burdaonelove@gmail.com>
2022-06-27 21:03:28 +0300

    feat: Разносит код из App.js на компоненты Main.js и Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <footer className="footer">
+                <p className="footer__copyright">
+                    &copy; 2020 Mesto Russia
+                </p>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;

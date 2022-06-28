1a0d6ee60dea960eb1ee812ff92f22a393101085
squaredbusinessman <burdaonelove@gmail.com>
2022-06-28 16:12:48 +0300

    feat: Настройка попапов. Выносит общий компонент PopupWithForm

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import PopupWithForm from "./PopupWithForm";
 
-const Main = () => {
+function Main() {
 
     function HandleEditAvatarClick() {
         document.querySelector('.popup_id_new-avatar').classList.add('popup_visible');
@@ -54,113 +55,90 @@ const Main = () => {
                     <ul className="cards"></ul>
                 </section>
 
+                <PopupWithForm name="profile-edit" title="Редактировать профиль">
+                    <label className="popup__label">
+                        <input
+                            type="text"
+                            className="popup__input popup__input_type_name"
+                            id="popup__input-nickname"
+                            name="name"
+                            aria-label="Поле ввода имени пользователя"
+                            minLength="2"
+                            maxLength="40"
+                            autoComplete="off"
+                            required
+                        />
+                        <span className="popup__input-error popup__input-nickname-error"></span>
+                    </label>
+                    <label className="popup__label">
+                        <input
+                            type="text"
+                            className="popup__input popup__input_type_about"
+                            id="popup__input-about"
+                            name="about"
+                            aria-label="Поле ввода информации о пользователе"
+                            minLength="2"
+                            maxLength="200"
+                            autoComplete="off"
+                            required
+                        />
+                        <span className="popup__input-error popup__input-about-error"></span>
+                    </label>
+                </PopupWithForm>
 
-                <div className="popup popup_id_profile-edit">
-                    <div className="popup__wrapper">
-                        <h2 className="popup__title">Редактировать профиль</h2>
-                        <form
-                            className="popup__form"
-                            name="editProfileForm"
-                            action="src/components/App#"
-                            method="post">
-                            <label className="popup__label">
-                                <input
-                                    type="text"
-                                    className="popup__input popup__input_type_name"
-                                    id="popup__input-nickname"
-                                    name="name"
-                                    aria-label="Поле ввода имени пользователя"
-                                    minLength="2"
-                                    maxLength="40"
-                                    autoComplete="off"
-                                    required
-                                />
-                                <span className="popup__input-error popup__input-nickname-error"></span>
-                            </label>
-                            <label className="popup__label">
-                                <input
-                                    type="text"
-                                    className="popup__input popup__input_type_about"
-                                    id="popup__input-about"
-                                    name="about"
-                                    aria-label="Поле ввода информации о пользователе"
-                                    minLength="2"
-                                    maxLength="200"
-                                    autoComplete="off"
-                                    required
-                                />
-                                <span className="popup__input-error popup__input-about-error"></span>
-                            </label>
-                            <button
-                                type="submit"
-                                className="popup__save-btn"
-                                aria-label="Кнопка отправки формы"
-                                disabled
-                            >
-                                Сохранить
-                            </button>
-                        </form>
-                        <button
-                            type="button"
-                            className="popup__close-btn"
-                            aria-label="Кнопка закрытия попапа"
-                        ></button>
-                    </div>
-                </div>
+                <PopupWithForm name="new-post" title="Новое место">
+                    <label className="popup__label">
+                        <input
+                            type="text"
+                            className="popup__input popup__input_type_name"
+                            id="popup__input-place-name"
+                            name="name"
+                            aria-label="Поле ввода названия"
+                            placeholder="Название"
+                            minLength="2"
+                            maxLength="30"
+                            autoComplete="off"
+                            required
+                        />
+                        <span className="popup__input-error popup__input-place-name-error"></span>
+                    </label>
+                    <label className="popup__label">
+                        <input
+                            type="url"
+                            className="popup__input popup__input_type_about"
+                            id="popup__input-src"
+                            name="link"
+                            aria-label="Поле ввода ссылки"
+                            placeholder="Ссылка на картинку"
+                            autoComplete="off"
+                            required
+                        />
+                        <span className="popup__input-error popup__input-src-error"></span>
+                    </label>
+                </PopupWithForm>
+
+                <PopupWithForm name="new-avatar" title="Обновить аватар">
+                    <label className="popup__label">
+                        <input
+                            className="popup__input popup__input_type-url"
+                            type="url"
+                            id="popup__input-avatar-url"
+                            name="newAvatarUrl"
+                            placeholder="Ссылка на картинку"
+                            aria-label="Поле ввода для ссылки на новый аватар"
+                            pattern="https?://.+"
+                            required
+                        />
+                        <span className="popup__input-error popup__input-avatar-url-error"></span>
+                    </label>
+                </PopupWithForm>
+
+                <PopupWithForm name="delete-confirm" title="Вы уверены?">
+                    ''
+                </PopupWithForm>
 
 
-                <div className="popup popup_id_new-post">
-                    <div className="popup__wrapper">
-                        <h2 className="popup__title">Новое место</h2>
-                        <form
-                            className="popup__form"
-                            name="newPostForm"
-                            action="src/components/App#"
-                            method="post">
-                            <label className="popup__label">
-                                <input
-                                    type="text"
-                                    className="popup__input popup__input_type_name"
-                                    id="popup__input-place-name"
-                                    name="name"
-                                    aria-label="Поле ввода названия"
-                                    placeholder="Название"
-                                    minLength="2"
-                                    maxLength="30"
-                                    autoComplete="off"
-                                    required
-                                />
-                                <span className="popup__input-error popup__input-place-name-error"></span>
-                            </label>
-                            <label className="popup__label">
-                                <input
-                                    type="url"
-                                    className="popup__input popup__input_type_about"
-                                    id="popup__input-src"
-                                    name="link"
-                                    aria-label="Поле ввода ссылки"
-                                    placeholder="Ссылка на картинку"
-                                    autoComplete="off"
-                                    required
-                                />
-                                <span className="popup__input-error popup__input-src-error"></span>
-                            </label>
-                            <button
-                                type="submit"
-                                className="popup__save-btn"
-                                aria-label="Кнопка отправки формы"
-                                disabled
-                            >
-                                Создать
-                            </button>
-                        </form>
-                        <button
-                            type="button"
-                            className="popup__close-btn"
-                            aria-label="Кнопка закрытия попапа"
-                        ></button>
-                    </div>
-                </div>
+
 
 
                 <div className="popup popup_id_big-picture">
@@ -178,69 +156,6 @@ const Main = () => {
                         ></button>
                     </div>
                 </div>
-
-
-                <div className="popup popup_id_delete-confirm">
-                    <form className="popup__form popup__form_type_confirm"
-                          name="deleteConfirmForm"
-                          action="src/components/App#"
-                          method="post">
-                        <h2 className="popup__title">
-                            Вы уверены?
-                        </h2>
-                        <button
-                            type="submit"
-                            className="popup__save-btn"
-                            aria-label="Кнопка отправки формы"
-                        >
-                            Да
-                        </button>
-                        <button
-                            type="button"
-                            className="popup__close-btn"
-                            aria-label="Кнопка закрытия попапа"
-                        ></button>
-                    </form>
-                </div>
-
-
-                <div className="popup popup_id_new-avatar">
-                    <form className="popup__form popup__form_type_avatar"
-                          name="updateAvatar"
-                          action="src/components/App#"
-                          method="post">
-                        <h2 className="popup__title">
-                            Обновить аватар
-                        </h2>
-                        <label className="popup__label">
-                            <input
-                                className="popup__input popup__input_type-url"
-                                type="url"
-                                id="popup__input-avatar-url"
-                                name="newAvatarUrl"
-                                placeholder="Ссылка на картинку"
-                                aria-label="Поле ввода для ссылки на новый аватар"
-                                pattern="https?://.+"
-                                required
-                            />
-                            <span className="popup__input-error popup__input-avatar-url-error"></span>
-                        </label>
-                        <button
-                            type="submit"
-                            className="popup__save-btn"
-                            aria-label="Кнопка отправки формы"
-                            disabled
-                        >
-                            Сохранить
-                        </button>
-                        <button
-                            type="button"
-                            className="popup__close-btn"
-                            aria-label="Кнопка закрытия попапа"
-                        ></button>
-                    </form>
-                </div>
-
             </main>
         </>
     );
---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,90 @@
+import React from 'react';
+
+/* Компонент для общего кода попапов:
+        - «Редактировать профиль» profile-edit
+        - «Новое место» new-post
+        - «Обновить аватар» new-avatar
+        - «Вы уверены?» delete-confirm
+
+
+*/
+
+function PopupWithForm(props) {
+
+    return (
+        <>
+            {
+                (props.name === 'profile-edit' || props.name === 'new-post')
+                ?
+                <div className={`popup popup_id_${props.name}`}>
+                    <div className="popup__wrapper">
+                        <h2 className="popup__title">{props.title}</h2>
+                        <form
+                            className="popup__form"
+                            name={props.name}
+                            action="src/components/App#"
+                            method="post">
+
+                            {props.children}
+
+                            <button
+                                type="submit"
+                                className="popup__save-btn"
+                                aria-label="Кнопка отправки формы"
+                                disabled
+                            >
+                                {
+                                    (props.name === 'profile-edit')
+                                        ?
+                                        `Сохранить`
+                                        :
+                                        `Создать`
+                                }
+                            </button>
+                        </form>
+                    <button
+                        type="button"
+                        className="popup__close-btn"
+                        aria-label="Кнопка закрытия попапа"
+                    ></button>
+                    </div>
+                </div>
+                :
+                <div className={`popup popup_id_${props.name}`}>
+                    <form className="popup__form popup__form_type_avatar"
+                          name={props.name}
+                          action="src/components/App#"
+                          method="post">
+                        <h2 className="popup__title">
+                            {props.title}
+                        </h2>
+
+                        {props.children}
+
+                        <button
+                            type="submit"
+                            className="popup__save-btn"
+                            aria-label="Кнопка отправки формы"
+                            disabled
+                        >
+                            {
+                                (props.name === 'new-avatar')
+                                    ?
+                                    `Сохранить`
+                                    :
+                                    `Да`
+                            }
+                        </button>
+                        <button
+                            type="button"
+                            className="popup__close-btn"
+                            aria-label="Кнопка закрытия попапа"
+                        ></button>
+                    </form>
+                </div>
+            }
+        </>
+    );
+};
+
+export default PopupWithForm;

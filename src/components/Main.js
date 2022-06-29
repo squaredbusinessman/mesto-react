de02452ca4c69ca7644b8b13b972eae2adfd9708
squaredbusinessman <burdaonelove@gmail.com>
2022-06-29 19:58:13 +0300

    feat: Подключите АПИ. Использует стейт для данных из АПИ

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,47 +1,80 @@
 import React from 'react';
+import api from "../utils/Api";
 
 function Main(props) {
 
-    return (
-        <>
-            <main>
-                <section className="user">
-                    <div className="user__avatar-wrapper">
-                        <img
-                            className="user__avatar"
-                            src="src/components/App#"
-                            alt="Аватарка пользователя"
-                        />
+    const [error, setError] = React.useState(null);
+    const [isLoaded, setIsLoaded] = React.useState(false);
+    const [user, setUserData] = React.useState({
+        name: '',
+        about: '',
+        avatar: '',
+    });
+
+    React.useEffect(() => {
+        api.getProfile().then(
+            (userData) => {
+                setIsLoaded(true);
+                setUserData({
+                    ...user,
+                    name: userData.name,
+                    about: userData.about,
+                    avatar: userData.avatar,
+                });
+            },
+            (error) => {
+                setIsLoaded(true);
+                setError(error);
+            }
+            )
+    }, );
+
+    if (error) {
+        return <div>Ошибка: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Загрузка...</div>;
+    } else {
+        return (
+            <>
+                <main>
+                    <section className="user">
+                        <div className="user__avatar-wrapper">
+                            <img
+                                className="user__avatar"
+                                src={user.avatar}
+                                alt="Аватарка пользователя"
+                            />
+                            <button
+                                onClick={props.onEditAvatar}
+                                type="button"
+                                className="user__avatar-editor-btn"
+                                aria-label="Кнопка редактирования аватара пользователя"
+                            ></button>
+                        </div>
+                        <div className="user__text-wrapper">
+                            <h1 className="user__name">{user.name}</h1>
+                            <button
+                                onClick={props.onEditProfile}
+                                type="button"
+                                className="user__nick-editor-btn"
+                                aria-label="Кнопка редактирования Имени и доп. информации пользователя"
+                            ></button>
+                            <p className="user__about">{user.about}</p>
+                        </div>
                         <button
-                            onClick={props.onEditAvatar}
+                            onClick={props.onAddPlace}
                             type="button"
-                            className="user__avatar-editor-btn"
-                            aria-label="Кнопка редактирования аватара пользователя"
+                            className="user__add-post-btn"
+                            aria-label="Кнопка создания новой публикации"
                         ></button>
-                    </div>
-                    <div className="user__text-wrapper">
-                        <h1 className="user__name">User-Name</h1>
-                        <button
-                            onClick={props.onEditProfile}
-                            type="button"
-                            className="user__nick-editor-btn"
-                            aria-label="Кнопка редактирования Имени и доп. информации пользователя"
-                        ></button>
-                        <p className="user__about">User-about</p>
-                    </div>
-                    <button
-                        onClick={props.onAddPlace}
-                        type="button"
-                        className="user__add-post-btn"
-                        aria-label="Кнопка создания новой публикации"
-                    ></button>
-                </section>
-                <section>
-                    <ul className="cards"></ul>
-                </section>
-            </main>
-        </>
-    );
-};
+                    </section>
+                    <section>
+                        <ul className="cards"></ul>
+                    </section>
+                </main>
+            </>
+        );
+    }
+}
 
 export default Main;
